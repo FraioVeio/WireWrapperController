ecb907261750b2571019def65057063adbd74827
FraioVeio <fraiomobile@gmail.com>
2019-05-06 17:10:24 +0200

    add Menu and EEPROM

--- WireWrapper_Arduino/WireWrapper_Arduino.ino
+++ WireWrapper_Arduino/WireWrapper_Arduino.ino
@@ -14,26 +14,27 @@
 #define D6 3
 #define D7 2
 
-#define DEBOUNCE_US 150000
+#define DEBOUNCE_US (long)200*1000
 
 
 /*
  * EEPROM Addresses
- * 0    - How many programs
- * 1    - First program length
- * 2    - First program type 0=angle 1=time
- * 3,6  - First instruction
- * 7,10 - Second instruction
- * ...
- * x    - Second program length
- * x+1  - Second program type
- * ...
+ * 
+ * Programmi lunghi 50 byte, totale 20 programmi
+ * 
+ * byte 0 = 0 0xS<lunghezza> dove S vale 0 se è controllato in tempo e 1 se in angoli;
+ * byte 1...49 angolo o tempo
  */
 
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 // Variabili per eventi dei pulsanti
 bool b1p, b2p, b3p, b4p;
 long b1m, b2m, b3m, b4m;
+
+//Variabili menu
+bool refreshMenu;
+bool insideProgram;
+int programId, totPrograms, programSize;
 
 void setup() {
   b1p = b2p = b3p = b4p;
@@ -51,6 +52,11 @@ void setup() {
   pinMode(BT_4, INPUT);
   pinMode(ENCODER, INPUT);
   
+  // Inizializzo menu
+  refreshMenu = insideProgram = false;
+  programId = 0;
+  totPrograms = countPrograms();
+  printMenu();
 }
 
 int bc = 0;
@@ -64,14 +70,29 @@ void b2_press() {
 }
 
 void b3_press() {
-  
+  if(!insideProgram) {
+    // Menu-
+    programId --;
+    if(programId <0 ) {
+      programId = totPrograms-1;
+    }
+    refreshMenu = true;
+  }
 }
 
 void b4_press() {
-  
+  if(!insideProgram) {
+    // Menu+
+    programId ++;
+    if(programId >= totPrograms) {
+      programId = 0;
+    }
+    refreshMenu = true;
+  }
 }
 
-
+boolean a = false;
+long t = 0;
 void loop() {
   // Gestione eventi pulsanti
   if(digitalRead(BT_1) && !b1p) {
@@ -116,7 +137,68 @@ void loop() {
   }
   // Fine eventi pulsanti
 
+  // Menu
+  if(refreshMenu)
+    printMenu();
   
+  if(Serial.available()) {
+    
+  }
+}
+
+void printMenu() {
+  refreshMenu = false;
+  if(!insideProgram) {
+    lcd.clear();
+    lcd.print("Programma ");
+    lcd.print(programId+1);
+    lcd.print("/");
+    lcd.print(totPrograms);
+    
+    lcd.setCursor(0, 1);
+    if(totPrograms != 0) {
+      byte a;
+      int count = 0;
+      for(int i=0;i<EEPROM.length();i+=50) {
+        if((a = EEPROM.read(i)) != 0) {
+          count ++;
+        }
+        if(count-1 == programId) {
+          break;
+        }
+      }
+      if(programType(a))
+        lcd.print("Angoli: ");
+      else
+        lcd.print("Tempi: ");
+
+      programSize = a & 0b01111111;
+      lcd.print(programSize);
+    }
+  } else {
+    // Inside program
+    
+  }
+}
+
+boolean programType(byte b) {
+  return (b & 0b10000000) != 0;
+}
+
+int countPrograms() {
+  int count = 0;
+  for(int i=0;i<EEPROM.length();i+=50) {
+    if(EEPROM.read(i) != 0) {
+      count ++;
+    }
+  }
+  return count;
+}
+
+void formatEeprom() {
+  for(int i=0;i<EEPROM.length();i++) {
+    EEPROM.write(i, 0);
+  }
 }
 
 void encoderInterrupt() {
